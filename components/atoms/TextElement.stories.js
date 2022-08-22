e962857647bc966a444409ec1bf31fc75b22009d
FollowJack <d.horvatic@outlook.com>
2022-08-22 17:43:04 +0200

    chore(Text): Update story

--- components/atoms/TextElement.stories.js
+++ components/atoms/TextElement.stories.js
@@ -1,12 +1,42 @@
+const variants = [
+  'headline1',
+  'headline2',
+  'headline3',
+  'copy-large',
+  'copy-medium',
+  'copy-small',
+]
+
+const colors = [
+  'primary',
+  'black',
+  'white',
+  'gray',
+  'light-gray',
+  'disabled',
+  'warning',
+  'error',
+  'success',
+]
+
 export default {
   title: 'Atoms/Text',
+  parameters: {
+    docs: {
+      description: {
+        component: `
+The Text component can be used anywhere text needs to be displayed. The use of the different type weights enables content to be clearly structured and pages to be showcased in an effective, diversified manner. It separates styling from semantics and enables flexibility.
+`
+      }
+    }
+  },
   argTypes: {
     default: {
       description: "The default Vue slot",
       control: {
         type: 'text',
       },
-      defaultValue: "Lorem Ipsum dolor sit amet",
+      defaultValue: "The quick brown fox jumps over the lazy dog.",
       table: {
         category: 'Slots',
         type: {
@@ -16,28 +46,11 @@ export default {
     },
 
     variant: {
-      options: [
-        'copy-large',
-        'copy-medium',
-        'copy-small',
-        'headline1',
-        'headline2',
-        'headline3',
-      ],
+      options: variants,
       control: { type: 'radio' },
     },
     color: {
-      options: [
-        'primary',
-        'black',
-        'white',
-        'gray',
-        'light-gray',
-        'disabled',
-        'warning',
-        'error',
-        'success',
-      ],
+      options: colors,
       control: { type: 'radio' },
     },
     weight: {
@@ -74,3 +87,39 @@ const Template = (args) => ({
 })
 export const Default = Template.bind({})
 Default.args = { ...defaultArgs }
+
+const variantArgs = {
+  color: 'black',
+  variant: "copy-medium",
+  weight: "normal",
+  as: "p",
+}
+
+
+const TemplateTypography = (args) => ({
+  data() {
+    return {
+      colors,
+      variants,
+    }
+  },
+  setup() {
+    return { args }
+  },
+  template:
+    `
+    <div class="bg-blue-100 space-y-4">
+      <div v-for="variant in variants" :key="variant">
+        {{variant}}:
+        <TextElement :variant="variant" v-bind="args">{{args.default}}</TextElement>
+      </div>
+      <div v-for="color in colors" :key="color">
+        {{color}}:
+        <TextElement :color="color" v-bind="args">{{args.default}}</TextElement>
+      </div>
+    </div>
+    `,
+})
+export const Typography = TemplateTypography.bind({})
+Typography.args = { ...variantArgs }
+
