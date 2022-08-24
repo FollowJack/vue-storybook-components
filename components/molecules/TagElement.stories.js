f5837217d29e93c5bcf511a27e4fb26ca5c4277a
FollowJack <d.horvatic@outlook.com>
2022-08-24 15:01:58 +0200

    chore(TagElement): Update story

--- components/molecules/TagElement.stories.js
+++ components/molecules/TagElement.stories.js
@@ -2,12 +2,36 @@ import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'Molecules/Tag',
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Tags are labels that contain small amounts of information that can be used in conjunction with each other to add context to other information on a page.
+Unlike a badge, they are not used to convey information that requires immediate attention and are not directly associated with another component.
+They often contain keywords that relate to another item, such as search results.
+
+# Best practices
+- Tags are used to visually label UI objects for quick recognition and navigation.
+- Tags increase the amount of visual noise, particularly when combined with other visual labelling elements, so try to use them in moderation.
+- Tags are designed to be displayed within a Tag Group.
+- Don't use tags to display critical, urgent information to a user.
+- When possible, keep UI related to adding/editing tags in close proximity to a tag group.
+`
+      }
+    }
+  },
   argTypes: {
     size: {
       options: [
         'small', 'medium', 'large',
       ],
       control: { type: 'radio' },
+    },
+    id: {
+      description: "The unique identifier",
+      control: {
+        type: 'text',
+      },
     },
   }
 }
@@ -19,7 +43,7 @@ const actionsData = {
 const defaultArgs = {
   size: "small",
   isRemovable: true,
-  id: '',
+  id: null,
 }
 
 const Template = (args) => ({
@@ -28,12 +52,38 @@ const Template = (args) => ({
   },
   template:
     `
-    <div class="bg-blue-100 space-x-1">
-      <TagElement v-bind="args" @click:remove="onClick">
+    <TagElement v-bind="args" @click:remove="onClick">
+      Vue.js
+    </TagElement>
+    `,
+})
+export const Default = Template.bind({})
+Default.args = { ...defaultArgs }
+
+
+const sizeArgs = {
+  isRemovable: true,
+  id: null,
+}
+
+const TemplateSizes = (args) => ({
+  setup() {
+    return { args, ...actionsData }
+  },
+  template:
+    `
+    <div class="space-x-1">
+      <TagElement size="small" v-bind="args" @click:remove="onClick">
+        Vue.js
+      </TagElement>
+      <TagElement size="medium" v-bind="args" @click:remove="onClick">
+        Vue.js
+      </TagElement>
+      <TagElement size="large" v-bind="args" @click:remove="onClick">
         Vue.js
       </TagElement>
     </div>
     `,
 })
-export const Default = Template.bind({})
-Default.args = { ...defaultArgs }
+export const Sizes = TemplateSizes.bind({})
+Sizes.args = { ...sizeArgs }
--- components/molecules/TagElement.vue
+++ components/molecules/TagElement.vue
@@ -17,38 +17,20 @@
       class=""
       @click:button="handleRemove"
     >
-      <svg
-        width="14"
-        height="14"
-        viewBox="0 0 14 14"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g
-          stroke="none"
-          stroke-width="1"
-          fill="none"
-          fill-rule="evenodd"
-          class="text-white"
-        >
-          <path
-            d="M1.00215385,13.0615384 L13.0593846,1 M1,1.00215385 L13.0615384,13.0593846"
-            stroke="currentColor"
-          ></path>
-        </g>
-      </svg>
+      <CloseIconElement class="text-white" />
     </ButtonElement>
   </span>
 </template>
 
 <script setup>
 import { computed } from 'vue'
+import CloseIconElement from '../atoms/CloseIconElement.vue'
 
 const emit = defineEmits(['click:remove'])
 const props = defineProps({
   id: {
     type: String,
-    default: '',
+    default: null,
   },
   isRemovable: {
     type: Boolean,
@@ -71,6 +53,6 @@ const textVariant = computed(() => {
 })
 
 function handleRemove() {
-  emit('click:remove')
+  emit('click:remove', props.id)
 }
 </script>
